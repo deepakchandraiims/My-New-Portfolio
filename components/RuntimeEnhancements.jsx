a89aebf1033e909a01fc5eb03ecd93d76b73cd42
deepakchandraiims <deepakchandraiims@gmail.com>
2026-08-22 19:58:41 +0530

    Use real Supabase certificate files and expose admin uploader

--- components/RuntimeEnhancements.jsx
+++ components/RuntimeEnhancements.jsx
@@ -6,12 +6,12 @@ import RecruiterBody from '@/components/RecruiterBody'
 import ProjectCatalogEnhancer from '@/components/ProjectCatalogEnhancer'
 import InvestmentLabEnhancement from '@/components/InvestmentLabEnhancement'
 import CertificateGallery from '@/components/CertificateGallery'
-import CertificateThumbnailEnhancer from '@/components/CertificateThumbnailEnhancer'
 import CertificateDocumentEnhancer from '@/components/CertificateDocumentEnhancer'
 
 const DirectSupabaseUploadBridge = dynamic(() => import('@/components/DirectSupabaseUploadBridge'), { ssr: false })
 const AdminWorkflowEnhancements = dynamic(() => import('@/components/AdminWorkflowEnhancements'), { ssr: false })
 const CertificationFileAdminEnhancer = dynamic(() => import('@/components/CertificationFileAdminEnhancer'), { ssr: false })
+const CertificateUploadDrawer = dynamic(() => import('@/components/CertificateUploadDrawer'), { ssr: false })
 const PortfolioEnhancements = dynamic(() => import('@/components/PortfolioEnhancements'), { ssr: false })
 const MarketEnhancements = dynamic(() => import('@/components/MarketEnhancements'), { ssr: false })
 
@@ -25,6 +25,7 @@ export default function RuntimeEnhancements() {
         <DirectSupabaseUploadBridge />
         <AdminWorkflowEnhancements />
         <CertificationFileAdminEnhancer />
+        <CertificateUploadDrawer />
         <PortfolioEnhancements />
         <MarketEnhancements />
       </>
@@ -37,7 +38,6 @@ export default function RuntimeEnhancements() {
       <ProjectCatalogEnhancer />
       <InvestmentLabEnhancement />
       <CertificateGallery />
-      <CertificateThumbnailEnhancer />
       <CertificateDocumentEnhancer />
     </>
   )
